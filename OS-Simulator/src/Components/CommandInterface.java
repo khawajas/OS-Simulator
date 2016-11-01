947a28878f7b4f26167bb1ac20d0de2a00bc6aef
Sherdil Khawaja <khawajas@vcu.edu>
2016-11-01 09:23:48 -0400

    beginnings...

---
+++ OS-Simulator/src/Components/CommandInterface.java
@@ -0,0 +1,30 @@
+package Components;
+
+public class CommandInterface {
+	
+	public void proc(){
+		
+	}
+		
+	public void mem(){
+			
+	}
+		
+	public void load(){
+			
+	}
+		
+	public void exe() {
+			
+	}
+		
+	public void reset() {
+			
+	}
+		
+	public void promptUser(){
+			
+	}
+		
+}
+
