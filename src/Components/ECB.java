92f4bf95551f61270611d9e27180b35208d67cfd
Sherdil Khawaja <khawajas@vcu.edu>
2016-11-07 21:47:32 -0500

    read the pdf and added the Event Control Block, and added more info on the event queue...

---
+++ src/Components/ECB.java
@@ -0,0 +1,10 @@
+package Components;
+
+/*The Event Control Block
+any event waiting service will be represented by an Event Control Block, and
+will wait for the service on EventQueue.  
+
+*/
+public class ECB {
+
+}
--- src/Components/EventQueue.java
+++ src/Components/EventQueue.java
@@ -1,8 +1,11 @@
 package Components;
 
 public class EventQueue {
-	//Encapsulates the event queue used by interrupt system.
-	
+	/*
+	 * Encapsulates the event queue used by interrupt system.
+	 * EventQueue will be a Priority Queue implemented as a Binary Heap data structure
+	 * 
+	 */
 	public void enQueue() {
 	    
 	}
