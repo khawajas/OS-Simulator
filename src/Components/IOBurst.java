f9b2bfab320456ac9e65847ae392f6c35cd888d1
Sherdil Khawaja <khawajas@vcu.edu>
2016-11-14 16:58:31 -0500

    the number of bursts added as comments...

--- src/Components/IOBurst.java
+++ src/Components/IOBurst.java
@@ -2,6 +2,7 @@ package Components;
 
 public class IOBurst {
 	//Randomly determines IO burst times
+	//the simulator shall have I/O operations taking between 25 and 50 cycles. The simulator runs in steps defined as “looped” cycles.
 	
 	public void generateIOBurst(){
 	
