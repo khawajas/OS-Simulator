2dd82a0e7cd8d63df3d992b53f5f6e51ce40e061
Sherdil Khawaja <khawajas@vcu.edu>
2016-11-17 17:47:10 -0500

    modified io burst...

--- src/Components/IOBurst.java
+++ src/Components/IOBurst.java
@@ -4,12 +4,12 @@ import java.util.Random;
 
 public class IOBurst {
 	//Randomly determines IO burst times
-	//the simulator shall have I/O operations taking between 25 and 50 cycles. The simulator runs in steps defined as “looped” cycles.
+	//the simulator shall have I/O operations taking between 25 and 50 cycles. The simulator runs in steps defined cycles.
+	private static int min = 25;
+	private static int max = 50;
 	
 	public int generateIOBurst(){
 		Random num = new Random();
-		int min = 25;
-		int max = 50;
 		int ioBurstTime = num.nextInt(max - min) + min;
 		return ioBurstTime;
 	}
