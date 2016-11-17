8ebf2cdf3512d9acc5a4f13fa268601de436a741
Sherdil Khawaja <khawajas@vcu.edu>
2016-11-17 17:58:43 -0500

    comments on i/o scheduler for chris...

--- src/Components/IOScheduler.java
+++ src/Components/IOScheduler.java
@@ -2,9 +2,15 @@ package Components;
 
 public class IOScheduler {
 	//Simulates I/O devices.
+	//uses FIFO algorithm
+	//Kernel schedules a set of I/O requests to determine a good order in which to execute them. 
+	//When an application issues a blocking I/O system call, the request is placed on the queue for that device. 
+	//The Kernel I/O scheduler rearranges the order of the queue to improve the overall system efficiency 
+	//and the average response time experienced by the applications.
+
 	
 	public void scheduleIO() {
-	    
+
 	}
 	  
 	public void startIO() {
